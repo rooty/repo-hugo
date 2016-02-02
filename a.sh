775469023b2d9e44655a8130ef7f9610ed7267a7
Vitaliy Mihnevich <rooty@chm.od.ua>
2016-02-03 00:38:21 +0200

    rebuilding site Wed Feb  3 00:38:21 EET 2016

--- a.sh
+++ a.sh
@@ -26,6 +26,6 @@ echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:rooty/rooty.github.io.git master
+#git subtree push --prefix=public git@github.com:rooty/rooty.github.io.git master
 
 echo -e "\033[0;32mChanges successfully published!\033[0m"
